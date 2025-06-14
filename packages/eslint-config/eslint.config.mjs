5ee1e24133b098bf715dbb35cb33215fa20da064
Thiago Rezende <thiago.manoel.rezende@gmail.com>
2025-06-13 21:37:12 -0300

    chore: remove unnecessary peer dependency from eslint-config and add typescript as peer to typescript-config

--- packages/eslint-config/eslint.config.mjs
+++ packages/eslint-config/eslint.config.mjs
@@ -1,15 +1,13 @@
 import { defineConfig } from 'eslint/config'
 
-/* globals */
-import globals from 'globals'
-
 /* plugins */
 import js from '@eslint/js'
-import stylistic from '@stylistic/eslint-plugin'
+import ts from 'typescript-eslint'
+import style from '@stylistic/eslint-plugin'
 
 /* configuration */
 export default defineConfig([
-  { files: ['**/*.{js,mjs}'], languageOptions: { globals: { ...globals.browser, ...globals.node } } },
-  { files: ['**/*.{js,mjs}'], plugins: { '@js': js }, extends: ['@js/recommended'] },
-  { files: ['**/*.{js,mjs}'], plugins: { '@stylistic': stylistic }, extends: ['@stylistic/recommended'] },
+  js.configs.recommended,
+  ts.configs.recommended,
+  style.configs.recommended,
 ])
